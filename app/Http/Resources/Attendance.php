30029f2629c000d5d56961b8b67ddc1e044230ba
Krishna W <ramain77@gmail.com>
2020-11-29 14:40:02 +0700

    format started_at & ended_at

--- app/Http/Resources/Attendance.php
+++ app/Http/Resources/Attendance.php
@@ -18,8 +18,8 @@ class Attendance extends JsonResource
             'id' => $this->id,
             'classroom_id' => $this->classroom_id,
             'user_id' => $this->user_id,
-            'started_at' => $this->started_at,
-            'ended_at' => $this->ended_at,
+            'started_at' => $this->started_at->format('H:i'),
+            'ended_at' => $this->ended_at->format('H:i'),
             'created_at' => $this->created_at->toFormattedDateString(),
             'updated_at' => $this->updated_at
         ];
