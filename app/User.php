5c3398c4f21dc8d88cc577e00122ca97ec61c795
Krishna W <ramain77@gmail.com>
2020-12-01 21:35:10 +0700

    add role for user

--- app/User.php
+++ app/User.php
@@ -37,6 +37,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
     public function classrooms()
     {
         return $this->hasMany(Classroom::class);
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,6 +15,7 @@ class UsersTableSeeder extends Seeder
     {
         User::create([
             'name' => 'Admin',
+            'role_id' => 1,
             'email' => 'admin@admin.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
