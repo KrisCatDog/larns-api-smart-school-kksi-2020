5932b03b49a2d458902136f6b322b2c76c4688b0
Krishna W <ramain77@gmail.com>
2020-12-03 21:29:55 +0700

    modify assignmentresult resource

--- app/Http/Resources/AssignmentResult.php
+++ app/Http/Resources/AssignmentResult.php
@@ -14,6 +14,14 @@ class AssignmentResult extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'user' => new User($this->user),
+            'attachment_file' => $this->attachment_file,
+            'file_name' => explode('/', $this->attachment_file)[6],
+            'score' => $this->score,
+            'created_at' => $this->created_at->toFormattedDateString(),
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
