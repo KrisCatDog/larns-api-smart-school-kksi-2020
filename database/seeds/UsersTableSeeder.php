cf9d0b9a34156dd41e6113b44c6cbe9d221873e2
Krishna W <ramain77@gmail.com>
2020-12-03 21:29:43 +0700

    add new seeder data

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,9 +14,18 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'Admin',
+            'name' => 'Teacher',
             'role_id' => 1,
-            'email' => 'admin@admin.com',
+            'email' => 'teacher@teacher.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'Student',
+            'role_id' => 2,
+            'email' => 'student@student.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
