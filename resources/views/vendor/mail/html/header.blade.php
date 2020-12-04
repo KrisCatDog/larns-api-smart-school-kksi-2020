9bf04adf5d1cfe52ec054e422b6ae80d51066eac
Krishna W <ramain77@gmail.com>
2020-12-04 21:32:10 +0700

    add mail vendor template

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}">
+<img src="{{ asset('img/larns-logo.svg') }}" style="width: 85px">
+<span style="coior: #333 !important; display: block; text-align: center; margin-top: 14px; font-weight: 400; font-size: 14px">The <span style="font-weight: 600; color: #f43f5e">Lightweight</span> Learning Management System</span>
+</a>
+</td>
+</tr>
